a22f9d3feb8967be8745a5c5014e782cd98d0e71
gborie <gborie7@gmail.com>
2018-07-05 19:33:01 +0100

    Create a class

---
+++ CS_Masterclass6/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Masterclass6
+{
+    // this class is a blueprint of a datatype
+    class Human
+    {
+        //member variable
+        public string firstName = "Michael";
+
+        // member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I am {0}", firstName);
+        }
+
+    }
+}
--- CS_Masterclass6/Program.cs
+++ CS_Masterclass6/Program.cs
@@ -10,6 +10,18 @@ namespace CS_Masterclass6
     {
         static void Main(string[] args)
         {
+            //Create an object of the class Human or an instance of Human
+            Human denis = new Human();
+            //access public variable from outside and even chnge it
+            denis.firstName = "Denis";
+            //call method of the class
+            denis.IntroduceMyself();
+
+            Human Michael = new Human();
+            Michael.firstName = "Mikel";
+            Michael.IntroduceMyself();
+
+            Console.ReadKey();
         }
     }
 }
